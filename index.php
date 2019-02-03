ad938a2f004f8a815f3006c7410ecab7164c682e
Scott Smith <scottsmith24@gmail.com>
2019-02-03 14:32:40 -0800

    new style listing item

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ get_template_part( 'template-parts/page-title' );
 
 echo '<main class="site-content">';
 
+echo '<p class="intro">'.the_content().'</p>';
+
+echo '<hr />';
+
 if (have_posts() ) {
 	while (have_posts() ) {
 		the_post();
