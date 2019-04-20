1a9fc44bdfa5976f3ed874f1b47cd168b4b212f2
Scott Smith <scottsmith24@gmail.com>
2019-04-20 11:48:15 -0700

    profile img

--- template-parts/home-header-content.php
+++ template-parts/home-header-content.php
@@ -1,10 +1,10 @@
 <section class="section-row">
     <div class="site-content home-hero-content">
         
-            <img src="<?php echo get_field('profile_image')['sizes']['medium']; ?>" class="profile-image alignright" />
+            <img src="https://i0.wp.com/smithscott.net/wp-content/uploads/2019/01/me.jpg?ssl=1" class="profile-image alignright" />
 
             <h1>Scott Smith</h1>
-            
+
             <p class="intro">My name is Scott Smith. I am web developer for California Lutheran University that specializes in front-end development and design.</p>
 
             <a href="portfolio" class="btn">View Portfolio</a>
