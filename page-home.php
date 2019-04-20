35a42e22a09252e74692aeea77b4ef28b9001fb9
Scott Smith <scottsmith24@gmail.com>
2019-04-20 11:10:58 -0700

    new style homepage

--- page-home.php
+++ page-home.php
@@ -1,10 +1,30 @@
 <?php get_header(); ?>
 
-<main class="site-content">
+<section class="section-row bg-gray">
+    <div class="site-content grid">
+        <div class="grid-col">
 
-    
+            <h2>Design & Code</h2>
+
+            <p>Any web site is bigger than the sum of it’s parts. A great site needs a strong front-end framework to drive the visual experience for the user. I believe in building projects that strive for visual aesthetics, accessibility, and a code base that works in favor of the user experience.</p>
+
+        </div>
+        <div class="grid-col">
+            
+            <h3>What I Work On</h3>
+
+            <ul class="featured-grid">
+                <li></li>
+            </ul>
+        </div>
+    </div>
+</section>
 
 
+<section class="section-row bg-gray">
+
+<div class="site-content">
+    <h2>Featured Work</h2>
 
     <?php
 
@@ -32,7 +52,42 @@
         
         wp_reset_postdata();
     ?>
+</div>
+</section>
 
-</main>
+<section class="section-row bg-gray">
+
+<div class="site-content">
+
+    <h2>From the Journal</h2>
+    
+    <?php
+
+        $query = new WP_Query(array(
+            'posts_per_page' => 4,
+            'order' => 'dsc'
+        ));
+
+        echo '<ul class="featured-grid">';
+
+    	while ( $query->have_posts() ) {
+		    $query->the_post(); ?>
+        
+            <li>
+                <a href="<?php the_permalink(); ?>" class="featured-grid-item">
+                    <?php the_post_thumbnail('thumbnail'); ?>
+                    <header><?php the_title(); ?></header>
+                </a>
+            </li>
+
+        <?php }
+        
+        echo '</ul>';
+        
+        wp_reset_postdata();
+    ?>
+</div>
+</section>
+
 
 <?php get_footer(); ?>
--- template-parts/home-header-content.php
+++ template-parts/home-header-content.php
@@ -1,11 +1,13 @@
-<div class="site-content">
+<section class="section-row">
+    <div class="site-content home-hero-content">
+        
+            <img src="<?php echo get_field('profile_image')['sizes']['medium']; ?>" class="profile-image alignright" />
 
-    <?php
-        // show regular post content
-        while(have_posts()){
-            the_post();
-            the_content(); 
-        }
-    ?>
+            <h1>Scott Smith</h1>
+            
+            <p class="intro">My name is Scott Smith. I am web developer for California Lutheran University that specializes in front-end development and design.</p>
+
+            <a href="portfolio" class="btn">View Portfolio</a>
 
     </div>
+</section>
