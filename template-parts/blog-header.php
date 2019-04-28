7b4e2c8b60a532b88d0f5024b7dedbcfc7370806
Scott Smith <scottsmith24@gmail.com>
2019-04-28 14:26:07 -0700

    added RSS icon

--- template-parts/blog-header.php
+++ template-parts/blog-header.php
@@ -1,6 +1,21 @@
-<header class="section-row page-title">
+<header class="section-row page-title blog-header">
     <div class="site-content">
         <div class="section-heading">Journal</div>
-        <a href="<?php bloginfo('rss_url'); ?>">RSS Feed</a>
+        
+        <nav class="blog-nav">
+            <ul class="horizontal-list">
+                <li>
+                    <a href="<?php bloginfo('rss_url'); ?>">
+                        <?php svg_icon('icon-rss'); ?>
+                        RSS Feed
+                    </a>
+                </li>
+                <li>
+                    <a href="<?php bloginfo('url'); ?>/archives">
+                        Archive
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </header>
