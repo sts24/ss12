3888320de10ffe8bd14674c299ad29a4dab89f43
Scott <stsmith@callutheran.edu>
2019-05-10 16:47:08 -0700

    external link post

--- template-parts/blog-posts-listing.php
+++ template-parts/blog-posts-listing.php
@@ -1,6 +1,6 @@
 <?php
 
-	$link = (get_field('URL') !== '') ? get_field('URL') : get_the_permalink();
+	$link = (get_field('URL') !== false) ? get_field('URL') : get_the_permalink();
 
 ?>
 
@@ -8,7 +8,11 @@
 <section class="col-s-12 margin-x-3 padding-bottom-3 blog-post blog-post-listing-item">
 
 	<h2 class="margin-top-0">
-		<?php if(get_field('URL') !== ''){ svg_icon('icon-link'); } ?>
+		<?php if(get_field('URL') !== ''){ 
+			echo '<div class="blog-link-icon">';
+			svg_icon('icon-link'); 
+			echo '</div>';
+		} ?>
 		<a href="<?php echo $link; ?>"><?php the_title(); ?></a>
 	</h2>
 	<?php the_content(); ?>
