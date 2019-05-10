3dcd8912f0c4f639b92adbe9ffc7309ecd1a68d3
Scott <stsmith@callutheran.edu>
2019-05-10 16:33:01 -0700

    link icon

--- template-parts/blog-posts-listing.php
+++ template-parts/blog-posts-listing.php
@@ -1,6 +1,16 @@
+<?php
+
+	$link = (get_field('URL') !== '') ? get_field('URL') : get_the_permalink();
+
+?>
+
+
 <section class="col-s-12 margin-x-3 padding-bottom-3 blog-post blog-post-listing-item">
 
-	<h2 class="margin-top-0"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	<h2 class="margin-top-0">
+		<?php if(get_field('URL') !== ''){ svg_icon('icon-link'); } ?>
+		<a href="<?php echo $link; ?>"><?php the_title(); ?></a>
+	</h2>
 	<?php the_content(); ?>
 
 </section>
