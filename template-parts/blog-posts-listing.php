f44e8602fffbb999e968342802ff8694c62c2d45
Scott <stsmith@callutheran.edu>
2019-05-10 14:38:53 -0700

    blog post listing margin

--- template-parts/blog-posts-listing.php
+++ template-parts/blog-posts-listing.php
@@ -1,6 +1,6 @@
-<section class="col-s-12 blog-post blog-post-listing-item">
+<section class="col-s-12 margin-x-3 padding-bottom-3 blog-post blog-post-listing-item">
 
-<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-<?php the_content(); ?>
+	<h2 class="margin-top-0"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	<?php the_content(); ?>
 
 </section>
