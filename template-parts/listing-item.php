36212764237f316a8b9995c8328b19bc254047e4
Scott <stsmith@callutheran.edu>
2019-05-16 14:33:47 -0700

    add button to portoflio items

--- template-parts/listing-item.php
+++ template-parts/listing-item.php
@@ -1,9 +1,10 @@
 <section class="listing-item">
     <div class="listing-image">
-    <a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail($post->id,'large'); ?></a>
+        <a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail($post->id,'large'); ?></a>
     </div>
     <div class="listing-content">
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <?php the_excerpt(); ?>
+        <a href="<?php the_permalink(); ?>" class="btn">More</a>
     </div>
 </section>
