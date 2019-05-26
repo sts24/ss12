cab5e8e4eefdfd93f2a3b2a49a9389404d7c2e76
Scott Smith <scottsmith24@gmail.com>
2019-05-26 14:14:47 -0700

    switch to gravatar for profile image, better text layout for home header

--- template-parts/home-header-content.php
+++ template-parts/home-header-content.php
@@ -1,13 +1,19 @@
 <section class="row padding-bottom-2">
-    <div class="col-s-12">
+    <div class="col-s-8">
         
-    <img src="https://i0.wp.com/smithscott.net/wp-content/uploads/2019/01/me.jpg?ssl=1" class="profile-image alignright" />
+    
 
-    <h1>Scott Smith</h1>
+    <h1 class="margin-bottom-1">Scott Smith</h1>
 
-    <p class="intro">My name is Scott Smith. I am web developer for California Lutheran University that specializes in front-end development and design.</p>
+    <p class="intro">My name is Scott Smith. I am web developer for California Lutheran University that specializes in front-end development and user experience design.</p>
 
     <a href="portfolio" class="btn">View Portfolio</a>
+
+    </div>
+
+    <div class="col-s-4 aligncenter">
+        <?php echo get_avatar('scottsmith24@gmail.com', 200); ?>
+    </div>
 
 </div>
 </section>
